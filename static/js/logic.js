3c5bf7c7739a38f45cc7152403beab18bace5a1f
Emma Dublin <emmadublin@Emmas-MacBook-Pro-8.local>
2022-06-09 23:23:41 -0500

    attempt to add legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,14 +15,16 @@ function createMap(earthquakes) {
     var customColor = "red";
     //Setting up color to change based on depth (third element in coordinates)
     for (var i = 0; i < earthquakes.length; i++){
-        if (earthquakes[i].geometry.coordinates[2] < 30)
-            customColor = "lime";
+        if (earthquakes[i].geometry.coordinates[2] < 10)
+            customColor = "#1FE511";
+        else if (earthquakes[i].geometry.coordinates[2] < 30)
+            customColor = "#98E511";
         else if (earthquakes[i].geometry.coordinates[2] < 50)
             customColor = "yellow";
         else if (earthquakes[i].geometry.coordinates[2] < 70)
             customColor = "orange";
         else if (earthquakes[i].geometry.coordinates[2] < 90)
-            customColor = "orange";
+            customColor = "#E53711";
 
         quakeMarkers.push(
             // Coordinates were backwards so needed to pull each value and reverse the order
@@ -61,7 +63,7 @@ function createMap(earthquakes) {
   // Create our map, giving it the streetmap and earthquakes layers to display on load.
     var myMap = L.map("map", {
         center: [
-          37.09, -95.71
+          40.0, -105.75
         ],
         zoom: 5,
         layers: [street, quakeLayer]
@@ -72,6 +74,15 @@ function createMap(earthquakes) {
         Earthquakes: quakeLayer
     };
 
+    function getColor(d) {
+        return d < 10 ? '#1FE511' :
+               d < 30 ? '#98E511' :
+               d < 50 ? 'yellow' :
+               d < 70 ? 'orange' :
+               d < 90 ? '#E53711' :
+                        'red';
+    }
+
   // Create a layer control.
   // Pass it our baseMaps and overlayMaps.
   // Add the layer control to the map.
@@ -79,6 +90,25 @@ function createMap(earthquakes) {
         collapsed: false
     }).addTo(myMap);
 
+//Attempt to add a legend in the bottom right
+    var legend = L.control({position: 'bottomright'});
 
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [ 10, 30, 50, 70, 90],
+            labels = [];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(map);
 
 }
